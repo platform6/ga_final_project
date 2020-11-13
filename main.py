d3fd8bed5e8198b77a8536fa1bb21edc3441d603
GarrettConn <36059331+platform6@users.noreply.github.com>
2020-11-13 15:18:43 -0500

    adding returned list

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class WordSearch:
     def __init__(self, search_word):
         self.search_word = search_word
         self.word_dict = self.find_related_words()
+        self.word_list = self.get_list_of_related_words()
 
     def find_related_words(self):
         word_dict = {}
@@ -15,12 +16,19 @@ class WordSearch:
         return word_dict
 
     def get_list_of_related_words(self):
-        pass
-
+        word_list = []
+        for key, value in self.word_dict.items():
+            inner_dict = value
+            for x, y in inner_dict.items():
+                if x != 'ant':
+                    for word in y:
+                        word_list.append(word)
+        return word_list
 
 def main():
     word = WordSearch('fire')
-    print(word.word_dict)
+    print(word.word_list)
+
 
 # the driver code
 if __name__ == '__main__':
